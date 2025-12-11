bb95ef6d5521a1b1c910e5ed2193e40751955fd3
kaname.takano <momijinn.aka@gmail.com>
2025-12-11 22:19:28 +0900

    Add astro-relative-links integration and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-
+import relativeLinks from 'astro-relative-links';
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [relativeLinks()],
+
   server: {
     port: 3000,
     open: true,
